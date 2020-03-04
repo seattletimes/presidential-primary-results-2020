b79962008f413d29c772dae32e2d189613c987ce
Hilary Fung <hfung@seattletimes.com>
2020-03-03 16:14:09 -0800

    county dropdown menu, layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 require("component-responsive-frame/child");
 
 var	all_statewide_results = require("../../data/results.json"),
-	statewide_results = all_statewide_results.races[0].results[0],
-	counties_by_fips = require("../../data/fips.sheet.json"),
-		all_county_results = require("../../data/results_counties.json"),
-	results = all_county_results[0].results,
-		all_candidates = require("../../data/candidates.sheet.json"),
-		candidates = all_candidates;
-	// candidates = all_candidates.filter(c => c.show === true);
+	all_county_results = require("../../data/results_counties.json");
+
+var statewide_results = all_statewide_results.races[0].results[0],
+	counties = require("../../data/fips.sheet.json"),
+	results_by_county = all_county_results[0].results,
+	candidates = require("../../data/candidates.sheet.json");
 
 var tooltip = document.querySelector("#tooltip"),
-	statewide_button = document.querySelector("#statewide-button"),
-	place_name = document.querySelector("#place-name"),
-	reporting_percentage = document.querySelector("#reporting-percentage"),
-	candidates_el = document.querySelector("#candidates");
+	county_selector = document.querySelector("#county-selector"),
+	county_results_el = document.querySelector("#county-results"),
+	county_name_el = document.querySelector("#county-name"),
+	reporting_percentage_el = document.querySelector("#reporting-percentage"),
+	candidates_el = document.querySelector("#county-results__candidates");
 
 var html = "";
 
@@ -22,40 +22,48 @@ var colors = {
 	"Warren": "#c45555",
 	"Buttigieg": "#d3b537",
 	"Biden": "#4995c4",
-	"Klobuchar": "#8766aa",
 	"Bloomberg": "#cc5c3e"
 };
 
-// var showStatewideResults = function() {
-// 	statewide_button.classList.remove("shown");
-// 	statewide_button.classList.add("hidden");
+var show_county_results = function(results) {
+	county_results_el.classList.remove("hidden");
+	county_results_el.classList.add("shown");
 
-// 	place_name.innerText = "Washington state";
-// 	reporting_percentage.innerText = statewide_results.reportingPercentage;
+	var county_name = counties[results.fips].county;
+	html = "";
+	candidates = candidates.sort(function(a, b) {
+		return results.candidates.find(c => c.last === b.last).votes
+			- results.candidates.find(c => c.last === a.last).votes;
+		});
+	candidates.forEach(function(c) {
+		var percentage = (results.candidates.find(a => a.last === c.last).percentage === null) ? 0 : results.candidates.find(d => d.last === c.last).percentage;
 
-// 	html = "";
-// 	candidates = candidates.sort(function(a, b) {
-//   return statewide_results.candidates.find(c => c.last === b.last).votes
-//   	- statewide_results.candidates.find(c => c.last === a.last).votes;
-// 	});
-// 	candidates.forEach(function(c) {
-// 		var percentage = (statewide_results.candidates.find(d => d.last === c.last).percentage === null) ? 0 : statewide_results.candidates.find(d => d.last === c.last).percentage;
-// 		html = html + "<li><span class='color-block " + c.last + "'></span>" + 
-// 			c.first + " " + c.last + 
-// 			": " + percentage + 
-// 			"%</li>";
-// 	});
+		html = html + "<p class='candidate " + 
+			(c.dropped_out ? "dropout'" : "'") + 
+			"><span class='color-block " + c.last + "'></span>" + 
+			c.last + 
+			": " + percentage + 
+			"%</p>";
+	});
 
-// 	candidates_el.innerHTML = html;
-// };
+	// county_name_el.innerText = county_name;
+	reporting_percentage_el.innerText = results.reportingPercentage;
+	candidates_el.innerHTML = html;
 
-results.forEach(function(county_results) {
-	var fips = county_results.fips;
-	var county_name = counties_by_fips[fips].county;
-	var el = document.querySelector("#" + counties_by_fips[fips].el_id);
-	var current_winner = county_results.candidates.sort(function(a, b) {
-		return b.votes - a.votes;
-	})[0];
+	county_selector.value = results.fips;
+};
+
+results_by_county.forEach(function(results) {
+	var fips = results.fips,
+		county_name = counties[fips].county,
+		el = document.querySelector("#" + counties[fips].el_id);
+	
+	var current_winner = "";
+	if (results.reportingPercentage > 0) {
+		current_winner = results.candidates.sort(function(a, b) {
+			return b.votes - a.votes;
+		})[0];
+	};
 
 	var fill = (colors[current_winner.last] ? colors[current_winner.last] : "rgb(224, 224, 224)")
 	el.setAttribute("fill", fill);
@@ -80,30 +88,13 @@ results.forEach(function(county_results) {
 	});
 
 	el.addEventListener("click", function() {
-		document.querySelector("#statewide-button").classList.remove("hidden");
-		document.querySelector("#statewide-button").classList.add("shown");
-
-		place_name.innerText = county_name;
-		reporting_percentage.innerText = county_results.reportingPercentage;
-
-		html = "";
-		candidates = candidates.sort(function(a, b) {
-			return county_results.candidates.find(c => c.last === b.last).votes
-  	- county_results.candidates.find(c => c.last === a.last).votes;
-		});
-		candidates.forEach(function(c) {
-			var percentage = (county_results.candidates.find(d => d.last === c.last).percentage === null) ? 0 : county_results.candidates.find(d => d.last === c.last).percentage;
-
-			html = html + "<li><span class='color-block " + c.last + "'></span>" + 
-				c.first + " " + c.last + 
-				": " + percentage + 
-				"%</li>";
-		});
-
-		candidates_el.innerHTML = html;
+		show_county_results(results);
 	});
 });
 
-// statewide_button.addEventListener("click", function() {
-// 	showStatewideResults();
-// });
+county_selector.addEventListener('change', function(e) {
+	var results = results_by_county.find(e => e.fips === this.value);
+	show_county_results(results);
+});
+
+show_county_results(results_by_county.find(e => e.fips === "53033"));
